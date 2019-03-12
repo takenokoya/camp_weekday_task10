dc385b35aa062a8e2774e78c372d631113379f73
takenokoya <takuya1002@gmail.com>
2019-03-12 19:24:49 +0900

    コントローラとルーティングの追加

---
+++ app/controllers/areas_controller.rb
@@ -0,0 +1,16 @@
+class AreasController < ApplicationController
+  def index
+  end
+
+  def show
+  end
+
+  def new
+  end
+
+  def create
+  end
+
+  def search
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  get 'areas/search' => 'areas#search'
+  resources :areas, :only => [ :index, :show, :new, :create ]
+  root 'areas#search'
 end
